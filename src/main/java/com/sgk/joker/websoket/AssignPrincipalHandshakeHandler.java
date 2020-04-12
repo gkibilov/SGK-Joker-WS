68927b093127f65a58f59327b34e21595a8ce5d1
George Kibilov <gkibilov@gmail.com>
2020-04-12 13:53:12 -0700

    cash user ids with session

--- src/main/java/com/sgk/joker/websoket/AssignPrincipalHandshakeHandler.java
+++ src/main/java/com/sgk/joker/websoket/AssignPrincipalHandshakeHandler.java
@@ -3,14 +3,18 @@ package com.sgk.joker.websoket;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.support.DefaultHandshakeHandler;
 
 import java.security.Principal;
+import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
 import java.util.Random;
 import java.util.Set;
+
+import javax.servlet.http.HttpSession;
 
 /**
  * Assign a random username as principal for each websocket client. This is
@@ -24,20 +28,49 @@ public class AssignPrincipalHandshakeHandler extends DefaultHandshakeHandler {
 	
 	private static Set<String> uniqueUserId = new HashSet<String>();
 	
+	private static Map<String, String> sessionTOUser = new HashMap<String, String>();
+	
+	
 	protected final Log logger = LogFactory.getLog("com.sgk.joker.websoket.AssignPrincipalHandshakeHandler");
 	
 	@Override
 	protected Principal determineUser(ServerHttpRequest request, WebSocketHandler wsHandler,
 			Map<String, Object> attributes) {
+		
 		final String name;
-		if (!attributes.containsKey(ATTR_PRINCIPAL)) {
-			name = generateUniqueUserId();
-			attributes.put(ATTR_PRINCIPAL, name);
-			logger.info("determineUser for principal " + request.getPrincipal() + " gennerated new id: " + name);
-		} else {
-			name = (String) attributes.get(ATTR_PRINCIPAL);
+		
+		HttpSession session = null;
+		
+		if (request instanceof ServletServerHttpRequest) {
+			ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
+			session = servletRequest.getServletRequest().getSession();
+			logger.info("determineUser for session id: " + session.getId());
+
+			if (!attributes.containsKey(ATTR_PRINCIPAL)) {
+				
+				if(sessionTOUser.get(session.getId()) != null) {
+					name = sessionTOUser.get(session.getId());
+					attributes.put(ATTR_PRINCIPAL, name);
+					logger.info("determineUser for principal " + request.getPrincipal() + " reuse id: " + name + " for session: " + session.getId());
+				}
+				else {	
+					name = generateUniqueUserId();
+					sessionTOUser.put(session.getId(), name);
+					attributes.put(ATTR_PRINCIPAL, name);
+					logger.info("determineUser for principal " + request.getPrincipal() + " gennerated new id: " + name);
+				}
+				
+			}
+			else {
+					name = (String) attributes.get(ATTR_PRINCIPAL);
+					logger.info("determineUser for principal " + request.getPrincipal() + " reuse id: " + name);
+			}
+		}
+		else {
+			name = "unknown";
 			logger.info("determineUser for principal " + request.getPrincipal() + " reuse id: " + name);
 		}
+		
 		return new Principal() {
 			@Override
 			public String getName() {
