b2747f894f78db4f056f676ccd230adc043c9419
George Kibilov <gkibilov@gmail.com>
2020-04-13 13:46:47 -0700

    enable HttpHandshakeInterceptor to handle new session id on refresh, handle only non null authToken

--- src/main/java/com/sgk/joker/websoket/HttpHandshakeInterceptor.java
+++ src/main/java/com/sgk/joker/websoket/HttpHandshakeInterceptor.java
@@ -2,15 +2,19 @@ package com.sgk.joker.websoket;
 
 import java.util.Map;
 
-import javax.servlet.http.HttpSession;
-
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
 import org.springframework.http.server.ServletServerHttpRequest;
 import org.springframework.web.socket.WebSocketHandler;
 import org.springframework.web.socket.server.HandshakeInterceptor;
+import  org.springframework.util.StringUtils;
 
 public class HttpHandshakeInterceptor implements HandshakeInterceptor {
+	
+	protected final Log logger = LogFactory.getLog("com.sgk.joker.websoket.HandshakeInterceptor");
+
 
 	@Override
 	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
@@ -19,8 +23,12 @@ public class HttpHandshakeInterceptor implements HandshakeInterceptor {
 			ServletServerHttpRequest servletRequest = (ServletServerHttpRequest) request;
 			//HttpSession session = servletRequest.getServletRequest().getSession();
 			//attributes.put("sessionId", session.getId());
-			attributes.put(AssignPrincipalHandshakeHandler.ATTR_PRINCIPAL, servletRequest.getServletRequest().getParameter("auth_token"));
-			return true;
+			String authToken = servletRequest.getServletRequest().getParameter("auth_token");
+			logger.info("HttpHandshakeInterceptor.beforeHandshake auth_token: " +  authToken);
+			if(!StringUtils.isEmpty(authToken)) {
+				attributes.put(AssignPrincipalHandshakeHandler.ATTR_PRINCIPAL, authToken);
+				return true;
+			}
 		}
 		return false;
 	}
